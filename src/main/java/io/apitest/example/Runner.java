fb7df0b51b6776bc136957a000e51125675d68dc
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-09-01 23:21:16 +0530

    configured test runner

--- src/main/java/io/apitest/example/Runner.java
+++ src/main/java/io/apitest/example/Runner.java
@@ -1,7 +1,23 @@
 package io.apitest.example;
 
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
 /**
  * Created by prasantabiswas on 01/09/18.
  */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/main/resources/features"},
+        strict = true,
+        glue = "io.apitest.example.stepsdefinition",
+        plugin = {"pretty",
+                "html:target/reports/cucumber/html",
+                "json:target/cucumber.json",
+                "usage:target/usage.jsonx",
+                "junit:target/junit.xml"}
+)
 public class Runner {
 }
